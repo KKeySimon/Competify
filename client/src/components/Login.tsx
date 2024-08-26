71a1dc5cab20165bbaf955c5a08f2073e5acc60d
KKeySimon <kkeysimon@gmail.com>
2024-08-26 13:44:54 +0800

    responsive login page

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,10 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { LoginProps } from "../../types";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Alert } from "react-bootstrap";
+
+interface LoginError {
+  username: string;
+  password: string;
+  apiError: string;
+}
 
 function Login({ isLoggedIn, setIsLoggedIn }: LoginProps) {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [errors, setErrors] = useState<LoginError>({
+    username: "",
+    password: "",
+    apiError: "",
+  });
 
   const navigate = useNavigate();
 
@@ -14,10 +28,28 @@ function Login({ isLoggedIn, setIsLoggedIn }: LoginProps) {
     }
   }, [isLoggedIn, navigate]);
 
+  const validateForm = () => {
+    const newErrors: LoginError = { username: "", password: "", apiError: "" };
+    if (!username) {
+      newErrors.username = "Username is required";
+    }
+    if (!password) {
+      newErrors.password = "Password is required";
+    }
+    return newErrors;
+  };
+
   async function handleLogin(e: React.FormEvent) {
     // prevents default behavior from occuring, which in form's case is redirecting to action URL in form
     e.preventDefault();
 
+    const formErrors = validateForm();
+
+    if (formErrors.username !== "" || formErrors.password !== "") {
+      setErrors(formErrors);
+      return;
+    }
+    setErrors({ username: "", password: "", apiError: "" });
     const userData = { username, password };
 
     await fetch("http://localhost:3000/api/login", {
@@ -31,16 +63,19 @@ function Login({ isLoggedIn, setIsLoggedIn }: LoginProps) {
     })
       .then((response) => {
         if (!response.ok) {
+          if (response.status === 401) {
+            throw new Error("Invalid Credentials");
+          }
           throw new Error("Something went wrong!");
         }
         return response.json();
       })
       .then((data) => {
         setIsLoggedIn(true);
-        console.log(data);
         console.log(data.message);
       })
       .catch((error) => {
+        setErrors({ ...errors, apiError: error.message });
         console.log(error.message);
       });
   }
@@ -48,23 +83,45 @@ function Login({ isLoggedIn, setIsLoggedIn }: LoginProps) {
   return (
     <div>
       <h1>This is the Login page!</h1>
-      <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Log In</button>
-      </form>
+      {errors.apiError && <Alert variant="danger">{errors.apiError}</Alert>}
+      <Form onSubmit={handleLogin}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => {
+              setErrors({ ...errors, username: "" });
+              setUsername(e.target.value);
+            }}
+            isInvalid={!!errors.username}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.username}
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setErrors({ ...errors, password: "" });
+              setPassword(e.target.value);
+            }}
+            isInvalid={!!errors.password}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.password}
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
     </div>
   );
 }
